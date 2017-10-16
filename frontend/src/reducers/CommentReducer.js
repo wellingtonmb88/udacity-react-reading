fbed5653a0147cb18189da465bf98ad7fc008f03
Wellington <wellingtonmb88@gmail.com>
2017-10-16 19:19:27 -0200

    Create reducer class for Comment

---
+++ frontend/src/reducers/CommentReducer.js
@@ -0,0 +1,50 @@
+import {
+    ADD_COMMENT,
+    UPDATE_COMMENT,
+    REMOVE_COMMENT,
+    UP_VOTE_COMMENT,
+    DOWN_VOTE_COMMENT
+} from '../actions/CommentActions'
+
+export function reducer(state = {}, action) {
+    const { comment, commentId } = action
+    switch (action.type) {
+
+        case ADD_COMMENT:
+        case UPDATE_COMMENT:
+            return {
+                ...state,
+                [comment.id]: comment
+            }
+
+        case REMOVE_COMMENT:
+            return state[commentId] ? {
+                ...state,
+                [commentId]: {
+                    ...state[commentId],
+                    deleted: true
+                }
+            } : state
+
+        case UP_VOTE_COMMENT:
+            return state[commentId] ? {
+                ...state,
+                [commentId]: {
+                    ...state[commentId],
+                    voteScore: state[commentId].voteScore + 1
+                }
+            } : state
+
+        case DOWN_VOTE_COMMENT:
+            return state[commentId] ? {
+                ...state,
+                [commentId]: {
+                    ...state[commentId],
+                    voteScore: state[commentId].voteScore - 1
+                }
+            } : state
+
+        default:
+            return state
+    }
+};
