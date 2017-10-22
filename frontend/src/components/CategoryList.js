bc82e3151ef113a985745e28adcab986e03a4431
Wellington <wellingtonmb88@gmail.com>
2017-10-22 00:48:18 -0200

    Updtae components

--- frontend/src/components/CategoryList.js
+++ frontend/src/components/CategoryList.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { List } from 'semantic-ui-react';
 import * as CategoryActions from '../actions/CategoryActions';
-import If from './If.js';
 
 class CategoryList extends Component {
+
+    componentDidMount() {
+        this.props.dispatch(CategoryActions.fetchCategories());
+    };
+
     render() {
         const { categories } = this.props;
         return (
             <div>
                 <h1>Categories</h1>
                 <List horizontal relaxed>
-                    {categories.items != undefined ? categories.items.map((item) => (
+                    {categories.items !== undefined ? categories.items.map((item) => (
                         <List.Item key={item.name}>
                             <List.Content>
                                 <List.Header as='a'>{item.name}</List.Header>
