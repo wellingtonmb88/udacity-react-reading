c9f2465d3a9f1162aa09bd335794ec2c0a994d85
Wellington <wellingtonmb88@gmail.com>
2017-10-22 00:31:00 -0200

    Update component to get categories from server API

--- frontend/src/components/CategoryList.js
+++ frontend/src/components/CategoryList.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { List } from 'semantic-ui-react';
+import * as CategoryActions from '../actions/CategoryActions';
+import If from './If.js';
 
-export default function CategoryList({ list }) {
-    return (
-        <div>
-            <h1>Categories</h1>
-            <List horizontal relaxed>
-                {list.map((item) => (
-                    <List.Item key={item}>
-                        <List.Content>
-                            <List.Header as='a'>{item}</List.Header>
-                        </List.Content>
-                    </List.Item>
-                ))}
-            </List>
-        </div>
-    )
+class CategoryList extends Component {
+    render() {
+        const { categories } = this.props;
+        return (
+            <div>
+                <h1>Categories</h1>
+                <List horizontal relaxed>
+                    {categories.items != undefined ? categories.items.map((item) => (
+                        <List.Item key={item.name}>
+                            <List.Content>
+                                <List.Header as='a'>{item.name}</List.Header>
+                            </List.Content>
+                        </List.Item>
+                    )) : null}
+                </List>
+            </div>
+        )
+    }
 };
+
+const mapStateToProps = (state) => ({
+    categories: state.categories
+});
+
+export default connect(
+    mapStateToProps
+)(CategoryList);
